7d499bb803c91591adbea9e17f2e52d56d268c34
Yundi339 <yundi339@foxmail.com>
2026-05-10 16:27:17 +0800

    feat: 安装脚本优化 (#638)
    * feat: 安装脚本优化
    
    * feat: 安装脚本优化
    
    * fix: update .gitattributes to ensure Windows batch scripts use CRLF line endings

--- src_assets/windows/misc/service/uninstall-service.bat
+++ src_assets/windows/misc/service/uninstall-service.bat
@@ -1,4 +1,5 @@
 @echo off
+set "PATH=%SystemRoot%\System32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SystemRoot%\System32\WindowsPowerShell\v1.0"
 setlocal enabledelayedexpansion
 
 set "SERVICE_CONFIG_DIR=%LOCALAPPDATA%\LizardByte\Sunshine"
